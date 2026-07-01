4db83f4b869713fb90afd513990b2c3b5395f22b
Cartnix <wh_winchester@mail.ru>
2026-07-01 23:16:29 +0500

    chore: ready for merge

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,16 +6,13 @@ import { ThemeToggle } from "@/shared/ui/components/ThemeToogle";
 
 const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
 
-export default function RootLayout({ children }: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html className={cn("font-sans", geist.variable)} suppressHydrationWarning>
-      <body className="min-h-full flex flex-col">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <html lang="en" className={cn("font-sans", geist.variable)} suppressHydrationWarning>
+      <body className="min-h-screen flex flex-col">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
-
-          <div className="fixed bottom-5 right-5 z-9999">
+          <div className="fixed bottom-5 right-5 z-50">
             <ThemeToggle />
           </div>
         </ThemeProvider>
