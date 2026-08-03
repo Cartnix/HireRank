2c24ac8a972936fc017af6b396fe6a72f789403f
Cartnix <wh_winchester@mail.ru>
2026-08-03 16:14:14 +0500

    fix: landing folder naming fixed

--- frontend/widgets/Landing/index.ts
+++
@@ -1,6 +0,0 @@
-export { CTA } from './CTA'
-export { Features } from './Features'
-export { Hero } from './Hero'
-export { HowItWorks } from './HowItWorks'
-export { Stats } from './Stats'
-export { Testimonial } from './Testimonial'
